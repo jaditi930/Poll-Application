3d98743a69db25da5860b205329c2fc85616cac9
Aditi Jain <jaditi930@gmail.com>
2023-09-25 01:00:12 +0530

    create polls page created

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home),
-    path('create_poll',views.create_poll,name="create_poll")
+    path('',views.home,name="home"),
+    path('create_poll',views.create_poll,name="create")
 ]
