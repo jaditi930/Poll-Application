bfcbc3e1efd95db2d3a03a3398718c8167f646b4
Aditi Jain <jaditi930@gmail.com>
2023-09-25 00:33:12 +0530

    added bootstrap

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.home),
+    path('create_poll',views.create_poll,name="create_poll")
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 def home(request):
-    return HttpResponse("hi user")
+    return render(request,"base.html")
+
+@csrf_exempt
+def create_poll(request):
+   return render(request,"create_poll.html") 
