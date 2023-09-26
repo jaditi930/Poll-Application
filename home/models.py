421da6bfd2856ad5eb4213f90bfdc7bb14818642
Aditi Jain <jaditi930@gmail.com>
2023-09-27 00:46:41 +0530

    added models

--- home/models.py
+++ home/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class PollQuestions(models.Model):
+
+    owner=models.ForeignKey(to=User,on_delete=models.CASCADE)
+    status=models.CharField(max_length=100)
+    question_value=models.CharField(max_length=100)
+
+
+class PollOptions(models.Model):
+
+    ques_id=models.ForeignKey(to=PollQuestions,on_delete=models.CASCADE)
+    opt_value=models.CharField(max_length=100)
+
+class PollResponses(models.Model):
+
+    user=models.ForeignKey(to=User,on_delete=models.CASCADE)
+    ques_id=models.ForeignKey(to=PollQuestions,on_delete=models.CASCADE)
+    opt_id=models.ForeignKey(to=PollOptions,on_delete=models.CASCADE)
+
+
+
+
+
+
+
