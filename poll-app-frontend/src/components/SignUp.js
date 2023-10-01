3f1dc9e855c6f88d80b7e063480478e10fb8f4f3
Aditi Jain <jaditi930@gmail.com>
2023-10-01 23:45:01 +0530

    new user signup done

--- poll-app-frontend/src/components/SignUp.js
+++ poll-app-frontend/src/components/SignUp.js
@@ -1,26 +1,55 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+const user={
+    email:"",
+    username:"",
+    password:""
 
-export default function Login()
+}
+function signup_user()
+{
+    fetch('http://127.0.0.1:7000/signin', {
+     
+    // Adding method type
+    method: "POST",
+    credentials:'include',
+     
+    // Adding body or contents to send
+    body: JSON.stringify(user),
+     
+    // Adding headers to the request
+    headers: {
+        "Content-type": "application/json; charset=UTF-8"
+    }
+})
+    .then((response)=>response.json())
+    .then((data)=>{
+        console.log(data)
+    })
+}
+export default function SignUp()
 {
     return (
         <>
     <Form>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control type="email" placeholder="Enter email" onChange={(e)=>{user.email=e.target.value}}/>
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicUsername">
         <Form.Label>Username</Form.Label>
-        <Form.Control type="text" placeholder="Enter username" />
+        <Form.Control type="text" placeholder="Enter username" onChange={(e)=>{user.username=e.target.value}}/>
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control type="password" placeholder="Password" onChange={(e)=>{user.password=e.target.value}}/>
       </Form.Group>
       
-      <Button variant="primary" type="submit">
+      <Button variant="primary" type="submit" onClick={(e)=>{
+        e.preventDefault()
+        signup_user()
+      }}>
         Submit
       </Button>
     </Form>
